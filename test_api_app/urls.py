d545ab4d5a43f2106068e46f852cc8a125f6a8b1
Shivam Dwivedi <shivamdwivedi2195@gmail.com>
2021-05-12 23:37:19 +0530

    create and login api for our profiles

--- test_api_app/urls.py
+++ test_api_app/urls.py
@@ -1,7 +1,15 @@
-from django.urls import path
+from django.urls import path, include
+
+from rest_framework.routers import DefaultRouter
 
 from test_api_app import views
 
+router = DefaultRouter()
+router.register('hello-viewset', views.HelloViewSets, base_name='hello-viewset')
+router.register('profile', views.UserProfileViewSet)
+
 urlpatterns = [
     path('hello-view/', views.HelloApiView.as_view()),
+    path('login/', views.UserLoginApiView.as_view()),
+    path('', include(router.urls))
 ]
--- test_api_app/views.py
+++ test_api_app/views.py
@@ -1,8 +1,15 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework import viewsets
+from rest_framework.authentication import TokenAuthentication
+from rest_framework import filters
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
 
 from test_api_app import serializer
+from test_api_app import models
+from test_api_app import permissions
 
 
 
@@ -32,7 +39,8 @@ class HelloApiView(APIView):
             message = f'hello {name}'
             return Response({'message':message})
         else:
-            return Response(serializerr.errors, status=status.HTTP_400_BAD_REQUEST)
+            return Response(serializerr.errors,
+            status=status.HTTP_400_BAD_REQUEST)
 
 
     def put(self, request, pk=None):
@@ -47,3 +55,65 @@ class HelloApiView(APIView):
     def delete(self, request, pk=None):
         """Delete an Object"""
         return Response({'method':'DELETE'})
+
+
+class HelloViewSets(viewsets.ViewSet):
+    """Test API viewsets"""
+
+    serializer_class = serializer.HelloSerializer
+
+    def list(self, request):
+        """Return a hello message"""
+        a_viewset = [
+            'Uses actions (list, create, retrieve, update, partial_update)',
+            'Automatically maps to URLs using routers',
+            'Provides more functionality with less code',
+        ]
+        return Response({'message':'Hello!', 'a_viewset':a_viewset})
+
+
+    def create(self, request):
+        """Create a new hello message"""
+
+        serializer_ =  self.serializer_class(data=request.data)
+
+        if serializer_.is_valid():
+            name = serializer_.validated_data.get('name')
+            message = f'Hello {name}'
+            return Response({'message': message})
+        else:
+            return Response(
+                serializer_.errors,
+                status=status.HTTP_400_BAD_REQUEST
+            )
+    def retrieve(self, request, pk=None):
+        """Handle getting an object by its ID"""
+        return Response({"http_method":'GET'})
+
+    def update(self, request, pk=None):
+        """Handle updating an object"""
+        return Response({'http_method': 'PUT'})
+
+    def patch(self, request, pk=None):
+        """Handle updating part of an object"""
+        return Response({'http_method': 'PATCH'})
+
+    def destroy(self, request, pk=None):
+        """Handle removing an object"""
+        return Response({'http_method': 'DELETE'})
+
+
+
+class UserProfileViewSet(viewsets.ModelViewSet):
+    """Handle ccreating and updating profiles"""
+    serializer_class = serializer.UserProfileSerializer
+    queryset = models.UserProfile.objects.all()
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (permissions.UpdateOwnProfile,)
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('name', 'email',)
+
+
+class UserLoginApiView(ObtainAuthToken):
+    """Handle creating user authentication token"""
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
